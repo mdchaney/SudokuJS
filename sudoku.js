f5ecfd1861f3bf6ca078eb479c61b69576e0083d
Michael Chaney <mdchaney@michaelchaney.com>
2017-09-07 18:43:17 -0500

    Updates with new data model.

--- sudoku.js
+++ sudoku.js
@@ -10,17 +10,43 @@ if (typeof Object.create !== 'function') {
 	}
 }
 
+// Each position on the puzzle is a "cell".  Each cell is a member
+// of three sets: row (y), column (x), and group.  The groups are typically
+// squares within the puzzle, but can be made of any contiguous set of
+// cells.  Traditionally, a puzzle is 9x9, with the group inset as sets
+// of cells each 3x3.
+//
+// Each cell must contain a value that is unique in all three sets of
+// which it is a part.
+
 // In following code, i is analogous to x, j is analogous to y
 var Cell = {
-	'box': null, 'x': null, 'y': null, 'value': null, 'div_id': null,
-	'showing': null, 'guesses': null, 'locked': null,
+	'puzzle': null,
+	'group': null,
+	'x': null,
+	'y': null,
+	'range': null,
+	'div_id': null,
+	'div': null,
+	'value': null,
+	'showing': null,
+	'guesses': null,
+	'locked': null,
+	'guess_rows': null,
 	'display_values': ['1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','0'],
-	'init': function(box, x, y, value) {
-		this.box = box;
+	'init': function(puzzle, x, y, range) {
+		this.puzzle = puzzle;
 		this.x = x;
 		this.y = y;
-		this.div_id = box.div_id + "_cell_" + x + "_" + y;
-		this.guesses = new Array(this.box.size*this.box.size);
+		this.range = range;
+		this.div_id = "cell_" + x + "_" + y;
+		this.guesses = new Array(range);
+		this.guess_rows = Math.ceil(Math.sqrt(range));
+	},
+	'set_group': function(group) {
+		this.group = group;
+	},
+	'set_value': function(value) {
 		this.value = value;
 	},
 	'value_from_display': function(display) {
@@ -32,8 +58,25 @@ var Cell = {
 		return null;
 	},
 	'display': function() {
-		$('<div class="cell" id="'+this.div_id+'"></div>').appendTo("#"+this.box.div_id);
-		$('#'+this.div_id).width(23).height(23).css('top',this.y*30+5).css('left',this.x*30+5).addClass('x_'+(this.x+this.box.x*this.box.size)).addClass('y_'+(this.y+this.box.y*this.box.size));
+		var td= $("<td id='" + this.div_id + "'></td>").addClass('cell').addClass('x_'+this.x).addClass('y_'+this.y).addClass('group_'+this.group.id);
+		if (this.x == 0 || this.puzzle.cell_at(this.x-1,this.y).group != this.group) {
+			td.addClass('ldiff');
+		}
+		if (this.x == this.range-1 || this.puzzle.cell_at(this.x+1,this.y).group != this.group) {
+			td.addClass('rdiff');
+		}
+		if (this.y == 0 || this.puzzle.cell_at(this.x,this.y-1).group != this.group) {
+			td.addClass('tdiff');
+		}
+		if (this.y == this.range-1 || this.puzzle.cell_at(this.x,this.y+1).group != this.group) {
+			td.addClass('bdiff');
+		}
+		this.div = td;
+		return td;
+	},
+	'show_marker': function() {
+		var marker = '' + this.group.id + "/" + this.x + "," + this.y;
+		$('#' + this.div_id).text(marker);
 	},
 	'clear': function() {
 		this.showing = null;
@@ -42,15 +85,39 @@ var Cell = {
 	'display_guesses': function() {
 		this.clear();
 		this.showing='guess';
-		for (var i=0; i<this.box.size*this.box.size; i++) {
+		for (var i=0; i<this.range; i++) {
 			if (this.guesses[i]) this.display_guess(i);
 		}
+	},
+	'guess_div_id': function(guess) {
+		return this.div_id + '_guess_' + guess;
 	},
 	'display_guess': function(guess) {
 		if (this.showing!==null && this.showing!=='guess') this.clear();
 		this.showing='guess';
-		$('<div class="guess" id="'+this.div_id+'_guess_'+guess+'">'+this.display_values[guess]+'</div>').appendTo('#'+this.div_id);
-		$('#'+this.div_id+'_guess_'+guess).css('top',Math.floor(guess/this.box.size)*8-1).css('left',(guess%this.box.size)*8+2);
+		var guess_row = Math.floor(guess/this.guess_rows);
+		var guess_col = guess % this.guess_rows;
+		var guess_div = $('<div class="guess" id="'+this.guess_div_id(guess)+'">'+this.display_values[guess]+'</div>')
+		var pixel_padding = 2;
+		var guess_width = guess_div.width();
+		var cell_width = this.div.width() - pixel_padding * 2;
+		var guess_height = guess_div.height();
+		var cell_height = this.div.height() - pixel_padding * 2;
+		if (guess_row == 0) {
+			guess_div.css('top', '' + pixel_padding + 'px');
+		} else if (guess_row == this.guess_rows - 1) {
+			guess_div.css('bottom', '' + pixel_padding + 'px');
+		} else {
+			guess_div.css('top', '' + (cell_height * guess_row / (this.guess_rows-1) - guess_height/2.0 + pixel_padding) + 'px');
+		}
+		if (guess_col == 0) {
+			guess_div.css('left', '' + pixel_padding + 'px');
+		} else if (guess_col == this.guess_rows - 1) {
+			guess_div.css('right', '' + pixel_padding + 'px');
+		} else {
+			guess_div.css('left', '' + (cell_width * guess_col / (this.guess_rows-1) - guess_width/2.0 + pixel_padding) + 'px');
+		}
+		guess_div.appendTo('#'+this.div_id);
 	},
 	'reveal': function() {
 		if (this.value!==null && this.showing!=='answer') {
@@ -67,42 +134,105 @@ var Cell = {
 	}
 };
 
-var Box = {
-	'x': null, 'y': null, 'size': null, 'div_id': null,
+var Group = {
+	'size': null,
+	'id': null,
 	'cells': null,
-	'init': function(x, y, size) {
-		this.x = x;
-		this.y = y;
+	'init': function(id,size) {
+		this.id = id;
 		this.size = size;
-		this.div_id = 'box_'+x+'_'+y;
-		this.cells = new Array(size);
+		this.cells = new Array();
+	},
+	'add_cell': function(cell) {
+		this.cells.push(cell);
+		cell.set_group(this);
+	}
+};
+
+var Sudoku = {
+	'size': null,
+	'groups': null,
+	'rows': null,
+	'cols': null,
+	'cells': null,
+	'group_map': null,
+	'current_number': null,
+	'element': null,
+	'init': function(size) {
+		this.size = size;
+		this.cells = new Array(size)
 		for (var i = 0; i < size; i++) {
 			this.cells[i] = new Array(size);
 			for (var j = 0; j < size; j++) {
-				var cell = Object.create(Cell);
-				cell.init(this, i, j, null);
-				this.cells[i][j] = cell;
+				this.cells[i][j] = Object.create(Cell);
+				this.cells[i][j].init(this,i,j,size);
+			}
+		}
+		this.groups = new Array(size);
+		for (var i = 0; i < size; i++) {
+			this.groups[i] = Object.create(Group);
+			this.groups[i].init(i, size);
+		}
+	},
+	'simple_groups': function() {
+		var s = Math.sqrt(this.size);
+		if (Math.floor(s) == s) {
+			this.group_map = new Array(this.size);
+			for (i=0 ; i<this.size ; i++) {
+				this.group_map[i] = new Array(this.size);
+				for (j=0 ; j<this.size ; j++) {
+					var group_number = Math.floor(i/s) * s + Math.floor(j/s);
+					this.group_map[i][j] = group_number;
+				}
+			}
+		} else {
+			throw new Error("Simple groups are only possible if size is the square of an integer.");
+		}
+	},
+	'gather_groups': function() {
+		for (i=0 ; i<this.size ; i++) {
+			for (j=0 ; j<this.size ; j++) {
+				var group_number = this.group_map[i][j];
+				this.groups[group_number].add_cell(this.cells[i][j]);
 			}
 		}
 	},
 	'make_simple': function() {
-		for (var x = 0 ; x < this.size ; x++) {
-			for (var y = 0 ; y < this.size ; y++) {
-				// (i*size+j+x+y*size)%(size*size)
-				this.cells[x][y].value = (x*this.size+y+this.x+this.y*this.size)%(this.size*this.size);
+		var s = Math.sqrt(this.size);
+		if (Math.floor(s) == s) {
+			for (var x = 0 ; x < this.size ; x++) {
+				for (var y = 0 ; y < this.size ; y++) {
+					var new_val = ((x % s) * s + Math.floor(x / s) + y) % this.size;
+					this.cells[x][y].set_value(new_val);
+				}
 			}
+		} else {
+			throw new Error("Simple groups are only possible if size is the square of an integer.");
 		}
 	},
 	'display': function(div_id) {
-		// create a new div - assume cells are 25x25 px with a 5px margin
-		var total_width = this.size * 30 + 5;
-		$('<div class="box" id="'+this.div_id+'"></div>').appendTo("#"+div_id);
-		$('#'+this.div_id).width(total_width).height(total_width).css('top',this.y*total_width).css('left',this.x*total_width);
-		if ((this.x+this.y*this.size)&1==1) $('#'+this.div_id).addClass('odd');
-		// Now create the cells
+		if (this.size) {
+			var table = $('<table class="sudoku"></table>');
+			for (j=0 ; j<this.size ; j++) {
+				var row = $("<tr></tr>");
+				for (i=0 ; i<this.size ; i++) {
+					var cell = this.cells[i][j];
+					var col = cell.display();
+					col.appendTo(row);
+				}
+				row.appendTo(table);
+			}
+			this.element = table;
+			table.appendTo($(div_id));
+		}
+	},
+	'cell_at': function(x,y) {
+		return this.cells[x][y];
+	},
+	'show_markers': function() {
 		for (var x=0 ; x < this.size; x++) {
 			for (var y=0 ; y < this.size; y++) {
-				this.cells[x][y].display();
+				this.cells[x][y].show_marker();
 			}
 		}
 	},
@@ -113,69 +243,10 @@ var Box = {
 			}
 		}
 	},
-	'clear': function() {
-		for (var x=0 ; x < this.size; x++) {
-			for (var y=0 ; y < this.size; y++) {
-				this.cells[x][y].clear();
-			}
-		}
-	}
-};
-
-var Sudoku = {
-	size: null,
-	'boxes': null,
-	'cells': null,
-	'current_number': null,
-	'init': function(size) {
-		this.size = size;
-		this.cells = new Array(size*size)
-		for (var i = 0; i < size*size; i++) {
-			this.cells[i] = new Array(size*size);
-		}
-		this.boxes = new Array(size);
-		for (var i = 0; i < size; i++) {
-			this.boxes[i] = new Array(size);
-			for (var j = 0; j < size; j++) {
-				var box = Object.create(Box);
-				box.init(i, j, size);
-				this.boxes[i][j] = box;
-				for (var x=0; x < size; x++) {
-					for (var y=0; y < size; y++) {
-						this.cells[i*size+x][j*size+y] = box.cells[x][y];
-					}
-				}
-			}
-		}
-	},
-	'make_simple': function() {
-		for (var x = 0 ; x < this.size ; x++) {
-			for (var y = 0 ; y < this.size ; y++) {
-				this.boxes[x][y].make_simple();
-			}
-		}
-	},
-	'display': function(div_id) {
-		if (this.size) {
-			// display each box, which in turn displays each cell
-			for (var x=0 ; x < this.size; x++) {
-				for (var y=0 ; y < this.size; y++) {
-					this.boxes[x][y].display(div_id);
-				}
-			}
-		}
-	},
-	'reveal': function() {
-		for (var x=0 ; x < this.size; x++) {
-			for (var y=0 ; y < this.size; y++) {
-				this.boxes[x][y].reveal();
-			}
-		}
-	},
 	'show_guesses': function() {
-		for (var x=0 ; x < this.size*this.size; x++) {
-			for (var y=0 ; y < this.size*this.size; y++) {
-				for (var guess=0 ; guess < this.size*this.size; guess++) {
+		for (var x=0 ; x < this.size; x++) {
+			for (var y=0 ; y < this.size; y++) {
+				for (var guess=0 ; guess < this.size; guess++) {
 					this.cells[x][y].guesses[guess] = true;
 				}
 				this.cells[x][y].display_guesses();
@@ -185,12 +256,12 @@ var Sudoku = {
 	'clear': function() {
 		for (var x=0 ; x < this.size; x++) {
 			for (var y=0 ; y < this.size; y++) {
-				this.boxes[x][y].clear();
+				this.cells[x][y].clear();
 			}
 		}
 	},
 	'create_controls': function(div_id) {
-		for (var i=0 ; i<size*size ; i++) {
+		for (var i=0 ; i<size ; i++) {
 			$('<a href="#" id="choose_' + i + '" class="chooser">' + Cell.display_values[i] + '</a>').appendTo('div#controls');
 			$('#choose_'+i).click(this.handle_number_click);
 		}
@@ -203,8 +274,8 @@ var Sudoku = {
 			if (!sudoku.current_number) { return; }
 			var reg;
 			// assignment on next line
-			if (reg=this.id.match(/box_(\d+)_(\d+)_cell_(\d+)_(\d+)/)) {
-				var cell=sudoku.boxes[parseInt(reg[1])][parseInt(reg[2])].cells[parseInt(reg[3])][parseInt(reg[4])];
+			if (reg=this.id.match(/group_(\d+)_(\d+)_cell_(\d+)_(\d+)/)) {
+				var cell=sudoku.groups[parseInt(reg[1])][parseInt(reg[2])].cells[parseInt(reg[3])][parseInt(reg[4])];
 				if (cell) {
 					if (event.shiftKey) {
 						cell.display_guess(sudoku.current_number);
